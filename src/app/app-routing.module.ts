d249a27ca4e95ed3173c343a17f56021537cbc5e
LiliRosales <90700177+LiliRosales@users.noreply.github.com>
2021-12-04 22:23:39 -0800

    Create pagina inicio y error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './plantillas/error/error.component';
+import { InicioComponent } from './plantillas/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    component: InicioComponent
+  },
+  {
+    path: "",
+    pathMatch: 'full',
+    redirectTo: '/inicio'
+  },
+  {
+    path: "**",
+    component: ErrorComponent
+  },
+  {
+    path: 'seguridad',
+    loadChildren: () =>import("./modulos/seguridad/seguridad.module").then(x =>x.SeguridadModule)
+  },
+  {
+    path: 'rentas',
+    loadChildren: () =>import("./modulos/rentas/rentas.module").then(x =>x.RentasModule)
+  },
+  {
+    path: 'administracion',
+    loadChildren: () =>import("./modulos/administracion/administracion.module").then(x =>x.AdministracionModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BannerNavegacionComponent } from './plantillas/banner-navegacion/banner-navegacion.component';
 import { FooterComponent } from './plantillas/footer/footer.component';
+import { InicioComponent } from './plantillas/inicio/inicio.component';
+import { ErrorComponent } from './plantillas/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerNavegacionComponent,
-    FooterComponent
+    FooterComponent,
+    InicioComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
