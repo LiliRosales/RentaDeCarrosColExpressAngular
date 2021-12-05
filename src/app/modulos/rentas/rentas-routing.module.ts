47e455885b70e972e3b8c9ba2c27a18e57b63e91
LiliRosales <90700177+LiliRosales@users.noreply.github.com>
2021-12-05 11:21:48 -0800

    Asignacion de rutas para modulo admin y rentas

--- src/app/modulos/rentas/rentas-routing.module.ts
+++ src/app/modulos/rentas/rentas-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AsignarSolicitudComponent } from './asignar-solicitud/asignar-solicitud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Asignar solicitud
+  {
+    path: 'asignar-solicitud',
+    component: AsignarSolicitudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
